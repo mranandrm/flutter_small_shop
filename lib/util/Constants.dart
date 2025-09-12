70ab8719695061d533019ce94518db933f77755d
Anand <rmanandmr05@gmail.com>
2025-09-12 10:44:12 +0530

    Create Constants.dart

---
+++ lib/util/Constants.dart
@@ -0,0 +1,8 @@
+class Constants {
+
+  static const String SERVER_DOMAIN ="http://192.168.1.98:8000";
+
+  static const String BASE_URL = SERVER_DOMAIN + "/api";
+
+  static const String CATEGORY_ROUTE ="/categories";
+}
